1f390d5c0136c0aa687676049888b10d1f18d3d0
Starishine <sania.amarnath@gmail.com>
2025-12-09 16:16:02 +0800

    Add signin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage'
+import SigninPage from './pages/SigninPage'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<SigninPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
