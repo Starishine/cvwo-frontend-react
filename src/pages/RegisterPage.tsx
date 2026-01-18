ead40a53eb8ba9f6199ab4ac74daad26e681f9fa
Starishine <sania.amarnath@gmail.com>
2026-01-18 21:07:58 +0800

    Modify register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -42,6 +42,7 @@ export default function RegisterPage() {
                 } else {
                     setError(data.error || 'Registration failed');
                 }
+                return;
             } else {
                 alert('Registration successful! Pls log in');
                 setUsername('');
